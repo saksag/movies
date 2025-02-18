8b63fc418ee49100619ef955ddffed53d4b4333e
saksag <sigdar1885@gmail.com>
2025-02-18 18:55:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,10 +19,10 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '8105750572:AAE36Op0-yN7cOgzRdLTu04wupekSDi
 ')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6736679432').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1718586743').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/sagar_optimizer") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002152624607'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+0mQkacVDTHgyMTk1')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002309621963').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
